50b22cf9b6c43e0c3093a1e2d373542436f693d9
Abhishek Salvi <abhisheksalviofficework@gmail.com>
2026-06-24 23:27:50 +0530

    feat: added hover card for hover on markers

--- components/MapCard.tsx
+++ components/MapCard.tsx
@@ -9,9 +9,19 @@ import {
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
+import {
+  Bike,
+  MapPin,
+  Package,
+  Warehouse,
+  CheckCircle2,
+  X,
+} from "lucide-react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
+import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
 
 const customIcon = new L.Icon({
   iconUrl: "/icons/pin.png", // put your icon in /public/icons
@@ -20,10 +30,11 @@ const customIcon = new L.Icon({
 });
 
 const locations = [
-  { id: 1, lat: 19.076, lon: 72.8777, label: "Mumbai" },
-  { id: 2, lat: 28.6139, lon: 77.209, label: "Delhi" },
+  { id: 1, lat: 19.076, lon: 72.8777, label: "Mumbai", renting: true },
+  { id: 2, lat: 28.6139, lon: 77.209, label: "Delhi", renting: false },
 ];
 const MapCard = () => {
+  const [selectedMarker, setSelectedMarker] = useState<object | null>(null);
   return (
     <>
       <Card className="overflow-hidden rounded-3xl">
@@ -42,6 +53,104 @@ const MapCard = () => {
         </CardHeader>
 
         <CardContent className="p-0">
+          <HoverCard open={!!selectedMarker} openDelay={10} closeDelay={100}>
+            <HoverCardContent
+              style={{
+                left: 300 + 20,
+                top: 300 - 20,
+              }}
+              className="w-[460px] absolute shadow-xl z-[9999]"
+            >
+              <Card className="w-[420px] rounded-3xl border-zinc-800 bg-zinc-950/95 backdrop-blur-md shadow-2xl">
+                <CardHeader className="">
+                  <div className="flex items-start justify-between">
+                    <CardTitle className="flex items-center gap-3 text-2xl">
+                      <MapPin className="h-6 w-6 text-red-500" />U Rathaus
+                      Schöneberg
+                    </CardTitle>
+                    <Button
+                      size="icon"
+                      variant="ghost"
+                      onClick={() => {
+                        setSelectedMarker(null);
+                      }}
+                    >
+                      <X className="h-5 w-5" />
+                    </Button>
+                  </div>
+                </CardHeader>
+
+                <Separator />
+
+                <CardContent className="space-y-3">
+                  {/* Stats */}
+                  <div className="space-y-4">
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center gap-3">
+                        <Bike className="h-5 w-5 text-blue-400" />
+                        <span>Bikes Available</span>
+                      </div>
+
+                      <span className="font-bold text-xl">0</span>
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center gap-3">
+                        <Warehouse className="h-5 w-5 text-amber-400" />
+                        <span>Empty Docks</span>
+                      </div>
+
+                      <span className="font-bold text-xl">7</span>
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center gap-3">
+                        <Package className="h-5 w-5 text-purple-400" />
+                        <span>Total Capacity</span>
+                      </div>
+
+                      <span className="font-bold text-xl">7</span>
+                    </div>
+
+                    <div className="flex items-center gap-3">
+                      <CheckCircle2
+                        className={`h-5 w-5 ${
+                          selectedMarker?.renting
+                            ? "text-green-500"
+                            : "text-red-500"
+                        }`}
+                      />
+
+                      <span
+                        className={`font-medium ${
+                          selectedMarker?.renting
+                            ? "text-green-500"
+                            : "text-red-500"
+                        }`}
+                      >
+                        {selectedMarker?.renting
+                          ? "Open for Renting"
+                          : "Closed"}
+                      </span>
+                    </div>
+                  </div>
+
+                  <Separator />
+
+                  {/* Address */}
+                  <div className="flex gap-3">
+                    <MapPin className="mt-1 h-5 w-5 text-red-500 shrink-0" />
+
+                    <p className="text-sm text-muted-foreground leading-6">
+                      Carl-Zuckmayer-Brücke, Schöneberger Dreieck, Bayerisches
+                      Viertel, Schöneberg, Tempelhof-Schöneberg, Berlin, 10825,
+                      Deutschland
+                    </p>
+                  </div>
+                </CardContent>
+              </Card>
+            </HoverCardContent>
+          </HoverCard>
           <div className="h-[550px]">
             <MapContainer
               center={[20.5937, 78.9629]}
@@ -58,6 +167,16 @@ const MapCard = () => {
                   key={loc.id}
                   position={[loc.lat, loc.lon]}
                   icon={customIcon}
+                  eventHandlers={{
+                    mouseover: () => {
+                      setSelectedMarker(loc);
+                      console.log("hovered", loc);
+                    },
+                    mouseout: () => {
+                      console.log("left", loc);
+                      // setSelectedMarker(null);
+                    },
+                  }}
                 >
                   <Popup>{loc.label}</Popup>
                 </Marker>
